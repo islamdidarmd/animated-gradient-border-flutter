1a478da3e12f2d264de8301f8f3348f41033ea52
Md Didarul islam <islamdidarmd@gmail.com>
2023-02-04 23:16:40 +0600

    Add drawing for gradient border

---
+++ lib/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'flutter_dash_image.dart';
+import 'gradient_border_paint.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Gradient Border"),
+      ),
+      body: Center(
+        child: SizedBox(
+          height: 100,
+          width: 100,
+          child: Stack(
+            children: [
+              const FlutterDashImage(),
+              const GradientBorderPaint(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
